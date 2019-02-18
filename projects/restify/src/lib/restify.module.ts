bc1efb1da2e88e8ea167cd0deef50c52e51b2222
Matheus Davidson <matheusdavidson@gmail.com>
2019-02-18 10:31:17 -0300

    feat(lib/restify): generate lib

---
+++ projects/restify/src/lib/restify.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RestifyComponent } from './restify.component';
+
+@NgModule({
+  declarations: [RestifyComponent],
+  imports: [
+  ],
+  exports: [RestifyComponent]
+})
+export class RestifyModule { }
---
+++ projects/restify/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of restify
+ */
+
+export * from './lib/restify.service';
+export * from './lib/restify.component';
+export * from './lib/restify.module';
